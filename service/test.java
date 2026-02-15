dc982fb80691d1931dccc5821702e415803be963
Aryan Raj <aryanraj1622@gmail.com>
2026-02-15 23:02:26 +0530

    final_push

---
+++ service/test.java
@@ -0,0 +1,14 @@
+package service;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class test {
+    @Test
+    void isStopWord_shouldReturnTrueForStopWord() {
+        assertTrue(true);
+    }
+
+    @Test
+    void isStopWord_shouldReturnFalseForNonStopWord() {
+        assertFalse(true);
+    }
+}
